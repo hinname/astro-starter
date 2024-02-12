2dc348d38b58e179b8bf4416ea3a806c8b664058
Hinkami <herivelton.pro@gmail.com>
2024-02-12 16:02:20 -0300

    preact added and rss.xml to ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-start-hinname.netlify.app/"
+  site: "https://astro-start-hinname.netlify.app/",
+  integrations: [preact()]
 });
