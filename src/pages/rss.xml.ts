066b6041252ff5c05318e559d898c17b3b82c752
Hinkami <herivelton.pro@gmail.com>
2024-02-13 16:54:05 -0300

    Posts using content collections

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,17 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import type { RSSFeedItem } from '@astrojs/rss';
+import { getAllPosts } from 'src/utils.astro';
 
 export async function GET(context: any) {
     const title = 'Astro Learner | Blog';
     const description = 'My journey learning Astro';
 
-    const items = await pagesGlobToRssItems(import.meta.glob('./**/*.md'));
+    const items = await getAllPosts();
     const itemsFixed: RSSFeedItem[] = items.map((item) => {
         const fixedItem = {
             ...item,
-            link: item.link ? item.link : context.site.toString(),
-            title: item.title ? item.title : title,
+            link: item.url,
+            title: item.title,
             pubDate: item.pubDate,
             description: item.description ? item.description : description,
         };
