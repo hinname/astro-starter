0adfa0901ba01972abeefb66a2a08eff87cb993b
hinname <herivelton.pro@gmail.com>
2024-02-15 18:22:57 -0300

    greetings component basic styles

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -7,10 +7,10 @@ export default function Greeting({messages}: {messages: string[]}) {
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div>
+    <div className='mt-6'>
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
-        New Greeting
+      <button className='mt-4 py-2 px-5 text-white rounded bg-violet-600 hover:bg-violet-800' onClick={() => setGreeting(randomMessage())}>
+        New Greeting!
       </button>
     </div>
   );
